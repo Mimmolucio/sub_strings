4980e8464673a70594562f84db5cf08b0a655b25
Mimmolucio <domenico.lp@gmail.com>
2021-03-21 00:12:17 +0100

    Finished, but not 100% correct --> last comment

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,11 +1,28 @@
+#require 'pry'
+
+#binding.pry
+
 dictionary = Array.new
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, subs)
+  word_array = Array.new
+  matches = Hash.new(0)
+  word_downcase = word.downcase.gsub(/[^a-z\s]/, '')    # make word downcase and remove punctuations
+  word_array = word_downcase.split(" ")
 
+  word_array.each do |compare_word|                     # the actual comparison
+   # matches[compare_word] = 0
+    if subs.include?(compare_word)
+      matches[compare_word] += 1
+    end
+  end
 
-
+  puts matches
 
 end
 
-substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+
+# ******* Works - but only for exact matches, not if a word of the dictionary is include in a word of the test-string. LOOK INTO .scan!!! ******* #
