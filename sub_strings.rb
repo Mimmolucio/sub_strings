aa35d344706c0948a7a7ced67b0af14956b71fb1
Mimmolucio <domenico.lp@gmail.com>
2021-03-20 11:26:16 +0100

    Setup of Method

---
+++ sub_strings.rb
@@ -0,0 +1,11 @@
+dictionary = Array.new
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, subs)
+
+
+
+
+end
+
+substrings("below", dictionary)
